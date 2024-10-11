67387e59caf2a3ca93f3f914b9069c12f5b3ec1b
Reynaldo Navedo <codigoderey@outlook.com>
2024-10-11 02:58:30 -0400

    updates to baseUrls

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,14 +1,5 @@
-let devUrl;
+let devUrl = "http://localhost:3000"
 let prodUrl = "https://4zimprov.com";
-
-
-if(window?.location?.href?.includes("localhost")) {
-	devUrl = "http://localhost:3000";
-} else {
-	devUrl = "https://eddies-project.vercel.app";
-}
-
-console.log(devUrl, prodUrl);
 
 export const baseURL =
 	process.env.NODE_ENV === "development"

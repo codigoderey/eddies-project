d65cbd9533f547e69388401a06eada1256a0f5be
Reynaldo Navedo <codigoderey@outlook.com>
2024-10-19 14:39:52 -0400

    updates to image gallery

--- src/components/StaticGallery.js
+++ src/components/StaticGallery.js
@@ -6,9 +6,11 @@ export default function StaticGallery({
   images,
   text = "Take a look at the beautiful work we can do for you. Go for the living that you deserve. We can help with that. If you want to contact us with any questions, please call 910-336-2054 or email info@4zimprov.com.",
 }) {
+
   useEffect(() => {
     initLightboxJS(process.env.NEXT_PUBLIC_LIGHTBOX_PRIVATE_KEY, "Individual");
-  }, []);
+
+  },[]);
 
   return (
     <section className="mt-16 mb-32">
@@ -39,21 +41,20 @@ export default function StaticGallery({
           }}
         >
           {images?.map((image) => (
-            <div className="relative" key={image.src}>
-              <Image
-                priority
-                src={image.src}
-                alt={image.alt}
-                height={500}
-                width={500}
-                data-lightboxjs="lightbox"
-                className="w-full h-full object-cover rounded-lg"
-                quality={100}
-              />
-              <div className="absolute bottom-0 left-0 text-black font-bold p-1 bg-white/70 rounded m-1">{image.alt}</div>
-            </div>
+            <Image
+              priority
+              key={image.src}
+              src={image.src}
+              alt={image.alt}
+              height={500}
+              width={500}
+              data-lightboxjs="lightbox"
+              className="w-full h-full object-cover rounded-lg relative"
+              quality={100}
+              
+            />
           ))}
-        </SlideshowLightbox>
+      </SlideshowLightbox>
       </div>
     </section>
   );

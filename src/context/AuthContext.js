50c41c0e08409eb9209c08fdf79e4598807c8e55
Reynaldo Navedo <codigoderey@outlook.com>
2024-11-14 19:40:31 -0500

    minor updates

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
 "use client";
-import nodemailer from "nodemailer";
+import { sendEmail } from "@/utils/sendEmail";
 import { createContext, useState } from "react";
 import {
 	getAuth,
@@ -23,28 +23,21 @@ export const AuthProvider = ({ children }) => {
 	const [user, setUser] = useState(null);
 	const auth = getAuth();
 
-	const sendEmailCode = async (email) => { 
-		try {
-			// 1. Generate the email link 
-			await sendSignInLinkToEmail(auth, email, actionCodeSettings);
-			window.localStorage.setItem("emailForSignIn", email);
-	
-			// 2. Send the custom email using Nodemailer
-			const emailLink = await auth.generateSignInWithEmailLink(email, actionCodeSettings); 
-			sendEmailWithNodemailer(email, emailLink); 
-	
-		} catch (error) {
-			const errorCode = error.code;
-			const errorMessage = error.message;
-			console.error(errorCode);
-			console.error(errorMessage);
-		}
+	const sendEmailCode = (email) => {
+		sendSignInLinkToEmail(auth, email, actionCodeSettings)
+			.then(() => {
+				window.localStorage.setItem("emailForSignIn", email);
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				console.error(errorCode);
+				console.error(errorMessage);
+			});
 	};
-	
 
 	const validateSignedUser = () => {
 		if (isSignInWithEmailLink(auth, window.location.href)) {
-
 			let email = window.localStorage.getItem("emailForSignIn");
 
 			if (!email) {
@@ -80,50 +73,6 @@ export const AuthProvider = ({ children }) => {
 			.catch((error) => {
 				console.error(error);
 			});
-	};
-
-	const sendEmailWithNodemailer = async (to, link) => {
-		// 1. Create a Nodemailer transporter
-		let transporter = nodemailer.createTransport({
-			host: process.env.MAIL_HOST,
-			port: 587,
-			secure: false, // true for 465, false for other ports
-			auth: {
-				user: process.env.MAIL_USER, // generated ethereal user
-				pass: process.env.MAIL_PASS // generated ethereal password
-			},
-			tls: {
-				rejectUnauthorized: false
-			}
-		});
-	
-		// 2. Define email options
-		let mailOptions = {
-			from: "4Z Improv <estimates@4zimprov.com>",
-			to: to,
-			subject: 'Sign in to Your App',
-			html: `
-				<!DOCTYPE html>
-				<html>
-				<head>
-				  <title>Sign in to 4Z Improv</title>
-				</head>
-				<body>
-				  <h1>Welcome to 4Z Improv</h1>
-				  <p>Click the link below to sign in:</p>
-				  <a href="${link}">${link}</a>
-				</body>
-				</html>
-			` 
-		};
-	
-		// 3. Send the email
-		try {
-			let info = await transporter.sendMail(mailOptions);
-			console.log("Message sent: %s", info.messageId);
-		} catch (error) {
-			console.error("Error sending email:", error);
-		}
 	};
 
 	return (
